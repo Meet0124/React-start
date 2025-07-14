fc197a36d7982e97da59ddffbdfa4372c752e1bd
Meet Adlakha <meet.adlakha@gmail.com>
2025-07-14 14:49:25 +0530

    Context of todo created

---
+++ 10_todoLocal/src/contexts/index.js
@@ -0,0 +1,3 @@
+// we created since we can have many context like todocontext and importing from different files is a tidious task so we import everything here and then export one single file 
+
+export {TodoProvider, TodoContext, useTodo} from "./TodoContext.js"
