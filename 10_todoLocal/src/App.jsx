160f8addebf48d64c94703174237b7188f34817c
Meet Adlakha <meet.adlakha@gmail.com>
2025-07-14 16:15:03 +0530

    Context of todo crud operations

--- 10_todoLocal/src/App.jsx
+++ 10_todoLocal/src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { TodoProvider } from "./contexts";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+      setTodos((prev) => [ {id: Date.now(), ...todo},...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id ? todo: prevTodo)))
+  }
+  const deleteTodo = (id) =>{
+    setTodos((prev)=> prev.filter((todo) => todo.id !== di ))
+  }
+  const toggleComplete = (id) =>{
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
 
   return (
-    <>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleTodo}}>
       
-    </>
+    </TodoProvider>
   )
 }
 
